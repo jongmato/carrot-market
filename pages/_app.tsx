f416023f80088fdaa5abda00e5c893b9a0159d64
jonghyun <jonghyeon2046@gmail.com>
2022-08-18 10:03:51 +0900

    refactor: 스타일 변경 & 컴포넌트 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="mx-auto w-full max-w-xl">
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
